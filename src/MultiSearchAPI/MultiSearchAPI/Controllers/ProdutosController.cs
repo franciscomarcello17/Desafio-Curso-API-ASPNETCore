9e7f3235e268a3b8915c91cad6e539fcd1dd1574
Francisco Marcello Ribeiro Lima <franciscomarcello17@gmail.com>
2025-02-13 22:24:14 -0300

    aprendendo criar API em ASP.NET Core e criando site

---
+++ src/MultiSearchAPI/MultiSearchAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+using MultiSearchAPI.Models;
+using MultiSearchAPI.Services;
+
+namespace MultiSearchAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ProdutoService _produtoService;
+
+        public ProdutosController()
+        {
+            _produtoService = new ProdutoService();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _produtoService.GetProdutos();
+            return Ok(produtos);
+        }
+    }
+}
---
+++ src/MultiSearchAPI/MultiSearchAPI/Services/ProdutoService.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using MultiSearchAPI.Models;
+
+namespace MultiSearchAPI.Services
+{
+    public class ProdutoService
+    {
+        private readonly string _filePath = "produtos.json";
+
+        public List<Produto> GetProdutos()
+        {
+            if (!File.Exists(_filePath)) return new List<Produto>();
+
+            string json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Produto>>(json) ?? new List<Produto>();
+        }
+    }
+}
